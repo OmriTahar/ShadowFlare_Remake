b7e9aed16b31f6eddc93a23c11cefdb695aa1221
Omri Tahar <omritaharnew@gmail.com>
2022-11-19 10:52:27 +0200

    Print index of mouse press - Works

--- ShadowFlare_Remake/Assets/_MyAssets/_Scripts/Inventory/InventoryController.cs
+++ ShadowFlare_Remake/Assets/_MyAssets/_Scripts/Inventory/InventoryController.cs
@@ -7,31 +7,21 @@ public class InventoryController : MonoBehaviour
 {
     public ItemsGrid CurrentHoveredItemsGrid;
 
-    //private void OnEnable()
-    //{
-    //    PlayerController.OnLeftMouseButtonPressed += leftMouseButtonWasPressed;
-    //}
+    private void OnEnable()
+    {
+        PlayerController.OnLeftMouseButtonPressed += LeftMouseButtonWasPressed;
+    }
 
-    //private void OnDisable()
-    //{
-    //    PlayerController.OnLeftMouseButtonPressed += leftMouseButtonWasPressed;
-    //}
+    private void OnDisable()
+    {
+        PlayerController.OnLeftMouseButtonPressed += LeftMouseButtonWasPressed;
+    }
 
-    private void Update()
+    private void LeftMouseButtonWasPressed()
     {
         if (CurrentHoveredItemsGrid == null)
             return;
 
-        //if ()
-        //{
-
-        //}
-
-        //print(SelectedItemsGrid.GetTileGridPosition(Mouse.current.position.ReadValue()));
+        print(CurrentHoveredItemsGrid.name + " was pressed at: " + CurrentHoveredItemsGrid.GetTileGridPosition(Mouse.current.position.ReadValue()));
     }
-
-    //private void leftMouseButtonWasPressed()
-    //{
-
-    //}
 }
