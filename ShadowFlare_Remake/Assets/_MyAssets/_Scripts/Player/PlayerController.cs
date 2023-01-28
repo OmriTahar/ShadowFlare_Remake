2b19775796643935268f2d02c4ab35c5e353bfb4
Omri Tahar <omritaharnew@gmail.com>
2023-01-28 11:08:03 +0200

    Refactoring player scripts

--- ShadowFlare_Remake/Assets/_MyAssets/_Scripts/Player/PlayerController.cs
+++ ShadowFlare_Remake/Assets/_MyAssets/_Scripts/Player/PlayerController.cs
@@ -7,7 +7,6 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-
     [Header("References")]
     public Texture2D[] CursorIconsArray;
     [SerializeField] private Player _player;
