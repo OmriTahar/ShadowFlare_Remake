f2fcb1a6c8ebdb3f0f7f728507b41e3ea2cc21a8
Omri Tahar <omritaharnew@gmail.com>
2024-10-25 17:45:18 +0300

    Cameras are fixed

--- ShadowFlare_Remake/Assets/_MyAssets/_Scripts/Cameras/CamerasView.cs
+++ ShadowFlare_Remake/Assets/_MyAssets/_Scripts/Cameras/CamerasView.cs
@@ -1,21 +1,53 @@
-using ShadowFlareRemake.Tools;
 using UnityEngine;
 
 namespace ShadowFlareRemake.Cameras
 {
     public class CamerasView : View<CamerasModel>
     {
+        [Header("General References")]
+        [SerializeField] private Transform _mainCameraTransform;
+        [SerializeField] private Transform _followTarget;
+
         [Header("Cameras")]
-        [SerializeField] private MultiStateView _camerasMSV;
+        [SerializeField] private Transform _centerCamera;
+        [SerializeField] private Transform _leftCamera;
+        [SerializeField] private Transform _rightCamera;
+
+        [Header("Settings")]
+        [SerializeField] private Vector3 _cameraOffset;
+
+        private void Update()
+        {
+            transform.position = _followTarget.position;
+        }
 
         protected override void ModelChanged()
         {
-            SetActiveCamera();
+            HandleSetActiveCamera();
         }
 
-        private void SetActiveCamera()
+        private void HandleSetActiveCamera()
         {
-            _camerasMSV.ChangeState((int)Model.CurrentActiveCamera);
+            switch (Model.CurrentActiveCamera)
+            {
+                case ActiveCamera.CenteredCamera:
+                    _mainCameraTransform.SetParent(_centerCamera);
+                    break;
+
+                case ActiveCamera.LeftCamera:
+                    _mainCameraTransform.SetParent(_leftCamera);
+                    break;
+
+                case ActiveCamera.RightCamera:
+                    _mainCameraTransform.SetParent(_rightCamera);
+                    break;
+
+                default:
+                    break;
+            }
+
+            _mainCameraTransform.localPosition = Vector3.zero;
+            _mainCameraTransform.localRotation = Quaternion.identity;
         }
     }
 }
